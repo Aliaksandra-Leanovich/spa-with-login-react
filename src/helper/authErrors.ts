b141f82d5b9adb9a90b9161db08411f9209c8bc4
«aleksandrics» <aliaksandraleanovich@gmail.com>
2022-12-08 14:19:39 +0300

    fix(app): fix folders and routes

--- src/helper/authErrors.ts
+++ src/helper/authErrors.ts
@@ -1,10 +1,3 @@
-import * as Yup from "yup";
-
-export const validationSchema = Yup.object().shape({
-  email: Yup.string().required("Email is required").email("Email is invalid"),
-  password: Yup.string().required("Password is required"),
-});
-
 export const getAuthError = (error: string) => {
   switch (error) {
     case "auth/user-not-found":
